60871c830f44d129caa0a48c7a1fbaf38aae45ff
SpringZz <wang90944@gmail.com>
2023-04-13 10:47:25 +0800

    添加冒泡排序的优化版本

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -2,13 +2,31 @@
  * 冒泡排序
  */
 let arr = [2, 3, 5, 1, 6, 4];
+// function bubbleSort(arr) {
+//   for (let i = 0; i < arr.length - 1; i++) {
+//     for (let j = 0; j < arr.length - i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+/**
+ * 优化版冒泡
+ */
 function bubbleSort(arr) {
-  for (let i = 0; i < arr.length - 1; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
+  let i = arr.length - 1;
+  while (i > 0) {
+    let post = 0;
+    for (let j = 0; j < i; j++) {
       if (arr[j] > arr[j + 1]) {
         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        post = j;
       }
     }
+    post = i;
   }
   return arr;
 }
